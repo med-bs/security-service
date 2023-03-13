573a9404f4e308eb6aea22d25fc069ec495aad3e
Mohamed Amine Ben Salem <abdbsalem@gmail.com>
2023-03-13 14:05:42 +0100

    basic authorisation with in memory user

---
+++ src/main/java/org/pfe/securityservice/security/Config.java
@@ -0,0 +1,38 @@
+package org.pfe.securityservice.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class Config {
+
+    @Bean
+    public UserDetailsService inMemoryUserDetailsManager(){
+        return new InMemoryUserDetailsManager(
+                // dans les requette authorisation doit etre code sur base64 (admin:1234)
+                User.withUsername("user1").password("{noop}1234").authorities("USER").build(),
+                User.withUsername("admin").password("{noop}1234").authorities("USER","ADMIN").build()
+        );
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeRequests(auth->auth.anyRequest().authenticated())
+                .sessionManagement(sess->sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .httpBasic(Customizer.withDefaults())
+                .build();
+    }
+
+}
